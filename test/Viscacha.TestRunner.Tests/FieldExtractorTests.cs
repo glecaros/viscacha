8541d2636df658751f2d762af4411f17aec7551f
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2025-04-23 21:26:21 +0000

    basic tests

---
+++ test/Viscacha.TestRunner.Tests/FieldExtractorTests.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Viscacha.TestRunner.Util;
+using Viscacha.Model;
+
+namespace Viscacha.TestRunner.Tests
+{
+    [TestFixture]
+    public class FieldExtractorTests
+    {
+        [Test]
+        public void ExtractFields_ReturnsExpectedValues_ForSimplePath()
+        {
+            var obj = new { foo = new { bar = 42 } };
+            var extractor = new FieldExtractor("$.foo.bar");
+            var result = extractor.ExtractFields<int>(obj);
+            Assert.That(result is Result<List<int>, Error>.Ok);
+
+            var extractedFields = result.Unwrap();
+
+            Assert.That(extractedFields, Has.Count.EqualTo(1));
+            Assert.That(extractedFields[0], Is.EqualTo(42));
+        }
+
+        [Test]
+        public void ExtractFields_ReturnsError_ForInvalidPath()
+        {
+            var obj = new { foo = 123 };
+            var extractor = new FieldExtractor("$.foo[");
+            var result = extractor.ExtractFields<int>(obj);
+
+            Assert.That(result is Result<List<int>, Error>.Err);
+
+            var error = result.UnwrapError();
+
+            Assert.That(error.Message, Does.Contain("Failed to parse JSON path"));
+        }
+    }
+}
---
+++ test/Viscacha.TestRunner.Tests/PathExtractorTests.cs
@@ -0,0 +1,20 @@
+using Viscacha.TestRunner.Util;
+
+namespace Viscacha.TestRunner.Tests
+{
+    [TestFixture]
+    public class PathExtractorTests
+    {
+        [Test]
+        public void ExtractPaths_ReturnsAllPaths_ForNestedObject()
+        {
+            var obj = new { foo = new { bar = 1, baz = 2 }, qux = 3 };
+            var wrapper = new ResponseWrapper(200, obj, []);
+            var extractor = new PathExtractor(wrapper);
+            var paths = extractor.ExtractPaths();
+            Assert.That(paths, Does.Contain("foo.bar"));
+            Assert.That(paths, Does.Contain("foo.baz"));
+            Assert.That(paths, Does.Contain("qux"));
+        }
+    }
+}
