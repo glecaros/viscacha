54c32eb2004ff57743483347edbf43d4a4c35592
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2025-05-01 13:51:24 -0700

    Enabling support for trx reports. (#31)

--- src/Viscacha.TestRunner/Program.cs
+++ src/Viscacha.TestRunner/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Testing.Platform.Builder;
 using dotenv.net;
 using Viscacha.TestRunner.Framework;
+using Microsoft.Testing.Extensions;
 
 DotEnv.Load();
 
@@ -10,8 +11,8 @@ builder.RegisterTestFramework(
     (_) => new TestingFrameworkCapabilities(),
     (_, serviceProvider) => new TestingFramework(serviceProvider));
 builder.CommandLine.AddProvider(() => new CommandLineOptions());
+builder.AddTrxReportProvider();
 
-// builder.TestHost.AddDataConsumer((serviceProvider) => new DisplayDataConsumer(serviceProvider.GetService<IOutputDevice>()!));
 var application = await builder.BuildAsync();
 
 return await application.RunAsync();
