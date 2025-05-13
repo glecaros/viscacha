3246e94a1e18c0aae94220beaf91776408502fb6
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2025-05-13 07:11:04 +0000

    Adding tests for bundle and multi-file schemas.

--- test/Viscacha.TestRunner.Tests/JsonSchemaValidatorTests.cs
+++ test/Viscacha.TestRunner.Tests/JsonSchemaValidatorTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using Viscacha.Model;
 using Viscacha.TestRunner.Framework;
@@ -9,10 +10,10 @@ using Viscacha.TestRunner.Model;
 namespace Viscacha.TestRunner.Tests;
 
 [TestFixture]
-public class JsonSchemaValidatorTests: TestBase
+public class JsonSchemaValidatorTests : TestBase
 {
     [Test]
-    public async Task ValidateAsync_PayloadMatches_ReturnsOk()
+    public async Task ValidateAsync_SelfContained_PayloadMatches_ReturnsOk()
     {
         FileInfo schemaFile = new("data/self-contained.json");
         if (!schemaFile.Exists)
@@ -20,7 +21,7 @@ public class JsonSchemaValidatorTests: TestBase
             throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
         }
 
-        Document doc = new (Defaults.Empty, []);
+        Document doc = new(Defaults.Empty, []);
         JsonSchemaValidation validation = new(new SelfContainedJsonSchema(schemaFile.FullName));
         JsonSchemaValidator validator = new(validation);
         var content = new
@@ -44,7 +45,7 @@ public class JsonSchemaValidatorTests: TestBase
     }
 
     [Test]
-    public async Task ValidateAsync_PayloadMismatch_ReturnsError()
+    public async Task ValidateAsync_SelfContained_PayloadMismatch_ReturnsError()
     {
         FileInfo schemaFile = new("data/self-contained.json");
         if (!schemaFile.Exists)
@@ -52,8 +53,162 @@ public class JsonSchemaValidatorTests: TestBase
             throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
         }
 
-        Document doc = new (Defaults.Empty, []);
+        Document doc = new(Defaults.Empty, []);
         JsonSchemaValidation validation = new(new SelfContainedJsonSchema(schemaFile.FullName));
+        JsonSchemaValidator validator = new(validation);
+        var content = new
+        {
+            firstName = "Bilbo",
+            age = 111,
+            nickNames = new[] { "Ringbearer", "Cousin Bilbo" },
+            address = new
+            {
+                street = "Bag End",
+                city = "Hobbiton",
+                country = "Shire",
+            },
+        };
+        List<TestVariantResult> testResults = [
+            new(new("v1", doc), [new(200, content, "application/json", [])])
+        ];
+        var result = await validator.ValidateAsync(testResults, default);
+        Assert.That(result is Result<Error>.Err);
+        var error = result.UnwrapError();
+        Assert.That(error.Message, Does.Contain("Validation failed for response"));
+    }
+
+    [Test]
+    public async Task ValidateAsync_Bundle_PayloadMatches_ReturnsOk()
+    {
+        FileInfo schemaFile = new("data/bundle.json");
+        if (!schemaFile.Exists)
+        {
+            throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
+        }
+
+        Document doc = new(Defaults.Empty, []);
+        JsonSchemaValidation validation = new(new BundleJsonSchema(schemaFile.FullName, "#/$defs/Person"));
+        JsonSchemaValidator validator = new(validation);
+        var content = new
+        {
+            firstName = "Bilbo",
+            lastName = "Baggins",
+            age = 111,
+            nickNames = new[] { "Ringbearer", "Cousin Bilbo" },
+            address = new
+            {
+                street = "Bag End",
+                city = "Hobbiton",
+                country = "Shire",
+            },
+        };
+        List<TestVariantResult> testResults = [
+            new(new("v1", doc), [new(200, content, "application/json", [])])
+        ];
+        var result = await validator.ValidateAsync(testResults, default);
+        Assert.That(result is Result<Error>.Ok);
+    }
+
+    [Test]
+    public async Task ValidateAsync_Bundle_PayloadMismatch_ReturnsError()
+    {
+        FileInfo schemaFile = new("data/bundle.json");
+        if (!schemaFile.Exists)
+        {
+            throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
+        }
+
+        Document doc = new(Defaults.Empty, []);
+        JsonSchemaValidation validation = new(new BundleJsonSchema(schemaFile.FullName, "#/$defs/Person"));
+        JsonSchemaValidator validator = new(validation);
+        var content = new
+        {
+            firstName = "Bilbo",
+            age = 111,
+            nickNames = new[] { "Ringbearer", "Cousin Bilbo" },
+            address = new
+            {
+                street = "Bag End",
+                city = "Hobbiton",
+                country = "Shire",
+            },
+        };
+        List<TestVariantResult> testResults = [
+            new(new("v1", doc), [new(200, content, "application/json", [])])
+        ];
+        var result = await validator.ValidateAsync(testResults, default);
+        Assert.That(result is Result<Error>.Err);
+        var error = result.UnwrapError();
+        Assert.That(error.Message, Does.Contain("Validation failed for response"));
+    }
+
+    [Test]
+    public async Task ValidateAsync_MultiFile_PayloadMatches_ReturnsOk()
+    {
+        FileInfo schemaFile = new("data/single-definitions/Person.json");
+        if (!schemaFile.Exists)
+        {
+            throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
+        }
+        List<FileInfo> dependencies =
+        [
+            new("data/single-definitions/Address.json"),
+            new("data/single-definitions/Car.json"),
+        ];
+        foreach (var dependency in dependencies)
+        {
+            if (!dependency.Exists)
+            {
+                throw new FileNotFoundException("Dependency file not found", dependency.FullName);
+            }
+        }
+
+        Document doc = new(Defaults.Empty, []);
+        JsonSchemaValidation validation = new(new MultiFileJsonSchema(schemaFile.FullName, [.. dependencies.Select(f => f.FullName)]));
+        JsonSchemaValidator validator = new(validation);
+        var content = new
+        {
+            firstName = "Bilbo",
+            lastName = "Baggins",
+            age = 111,
+            nickNames = new[] { "Ringbearer", "Cousin Bilbo" },
+            address = new
+            {
+                street = "Bag End",
+                city = "Hobbiton",
+                country = "Shire",
+            },
+        };
+        List<TestVariantResult> testResults = [
+            new(new("v1", doc), [new(200, content, "application/json", [])])
+        ];
+        var result = await validator.ValidateAsync(testResults, default);
+        Assert.That(result is Result<Error>.Ok);
+    }
+
+    [Test]
+    public async Task ValidateAsync_MultiFile_PayloadMismatch_ReturnsError()
+    {
+        FileInfo schemaFile = new("data/single-definitions/Person.json");
+        if (!schemaFile.Exists)
+        {
+            throw new FileNotFoundException("Schema file not found", schemaFile.FullName);
+        }
+        List<FileInfo> dependencies =
+        [
+            new("data/single-definitions/Address.json"),
+            new("data/single-definitions/Car.json"),
+        ];
+        foreach (var dependency in dependencies)
+        {
+            if (!dependency.Exists)
+            {
+                throw new FileNotFoundException("Dependency file not found", dependency.FullName);
+            }
+        }
+
+        Document doc = new(Defaults.Empty, []);
+        JsonSchemaValidation validation = new(new MultiFileJsonSchema(schemaFile.FullName, [.. dependencies.Select(f => f.FullName)]));
         JsonSchemaValidator validator = new(validation);
         var content = new
         {
